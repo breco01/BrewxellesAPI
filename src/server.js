33d62d18b1c327188fddc25253c634de46dd2f50
Brent Cornet <brent.cornet@telenet.be>
2025-08-09 23:18:23 +0200

    MongoDB Setup

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { connectDB } from './db.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  await connectDB(process.env.MONGODB_URI);
+
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
